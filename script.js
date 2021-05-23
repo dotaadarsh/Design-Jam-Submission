9003409760ed9de2e9061ad13785ce1d75e9c772
madmax-ak <71810927+madmax-ak@users.noreply.github.com>
2021-05-23 13:18:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// https://github.com/VoloshchenkoAl/revealer
+document.addEventListener('DOMContentLoaded', () => {
+  const revealerNav = window.revealer({
+    revealElementSelector: '.nav-js',
+    options: {
+      anchorSelector: '.nav-btn-js',
+    },
+  });
+
+  const actionBtn = document.querySelector('.nav-btn-js');
+  actionBtn.addEventListener('click', () => {
+    if (!revealerNav.isRevealed()) {
+      revealerNav.reveal();
+      actionBtn.setAttribute('data-open', true);
+    } else {
+      revealerNav.hide();
+      actionBtn.setAttribute('data-open', false);
+    }
+  });
+});
